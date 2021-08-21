3708baf6b08dca2d59cab9357de50da38bbc0537
Teguh Prasetyo <teguhpraz715@gmail.com>
2021-08-21 21:38:17 +0700

    make it better

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/ini', (req, res) => {
+    res.send('Hello Mas Teguh');
+});
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
