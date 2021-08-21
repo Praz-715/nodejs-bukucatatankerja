60d304ab8c0af92f5b628f7e978bd5825071f922
Teguh Prasetyo <teguhpraz715@gmail.com>
2021-08-21 21:35:46 +0700

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 });
 
 //Run app, then load http://localhost:3000 in a browser to see the output.
