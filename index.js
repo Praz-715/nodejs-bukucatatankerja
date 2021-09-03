c225fdc03d7b9a3a0ced9b86261beed31af80425
Teguh Prasetyo <teguhpraz715@gmail.com>
2021-09-03 20:47:05 +0700

    add favicon

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 // SetUp Flash
 app.use(cookieParser('secret'));
